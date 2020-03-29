efa1b6c1e97f41fe0f0c3be45f8b2e332de1505c
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2020-03-29 20:04:15 +0200

    feat: add jrepl '/save -history' meta command result file

---
+++ com/rwaw/java9/repl/jshell_session1.java
@@ -0,0 +1,69 @@
+F
+\f()
+f()
+void f(int i) {
+int a = i + 10;
+System.out.println("Your number + 10 is: " + a);
+return a;
+}
+void f(int i) {
+System.out.println("Your number + 10 is: " + a);
+}
+f
+f()
+f(10)
+void f222() {
+}
+f222()
+f(10)
+int d = 10;
+f(d)
+void f(int i) {
+System.out.println("Your number + 10 + secret is: " i + a + 10);
+}
+void f(int i) {
+System.out.println("Your number + 10 + secret is: " + i + a + 10);
+}
+f(111)
+int a = 666
+f(111)
+String string = "string"
+String another = 
+"" +
+"a" +_
+String another = 
+"" +
+"a" +
+" " +
+"b"
+/
+/import
+/help
+/list
+/vars
+/list -all
+/list -start
+/help
+/save sample/java
+/save sample.java
+ls
+/save sample22.java
+/save sample/java
+/save sample/java
+/save sample.java
+/LIST
+/list
+/list -all
+/save c:\sample.java
+/save c:\\sample.java
+/help /save
+/save -history
+/save -history jshell_session1
+/save -history jshell_session1.java
+/save -history ./jshell_session1.java
+/save -history .\jshell_session1.java
+/save -history c:\\jshell_session1.java
+/save -history 'c:\\jshell_session1.java'
+/save -history 'C:\Users\ext-wawrra\__workshop\BLS\deep-dive-int-java-sessions\java9-REPL\jshell_session1.java'
+/save -history 'C:\\Users\ext-wawrra\__workshop\BLS\deep-dive-int-java-sessions\java9-REPL\jshell_session1.java'
+/save -history 'C:\\Users\\ext-wawrra\\__workshop\\BLS\\deep-dive-int-java-sessions\\java9-REPL\\jshell_session1.java'
